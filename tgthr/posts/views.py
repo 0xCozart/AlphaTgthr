95068e105e50c8744e0bc7e417b090258bf676f9
Alan <alanvega002@gmail.com>
2020-05-11 01:04:26 -0800

    Post model and template added

--- tgthr/posts/views.py
+++ tgthr/posts/views.py
@@ -1,9 +1,33 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
-
+from django.urls import path, re_path # url()
 from .models import Post
 # Create your views here.
 
-def home_view(request, post_id, *args, **kwargs):
-    obj = Post.objects.get (id=post_id)
-    return HttpResponse(f"<h1>{post_id} - {obj.content}</h1>")
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    return render(request, "pages/home.html", context={}, status=200)
+
+
+def post_detail_view(request, post_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by Javascript or Swift/Java/iOS/Android
+    return json data
+    """
+    print(post_id)
+    data = {
+        "id": post_id,
+        # "content": obj.content,
+        # "image_path": obj.image.url 
+    }
+    status = 200
+    try:
+        obj = Post.objects.get(id=post_id)
+        data['content'] = obj.content
+    except: 
+        data['message'] = "Not Found"
+        status = 404
+    
+    # return HttpResponse(f"<h1>{post_id} - {obj.content}</h1>")
+    return JsonResponse(data, status=status) #Json.dumps content_type="application/json"
--- tgthr/tgthr/urls.py
+++ tgthr/tgthr/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from posts.views import home_view
+from posts.views import home_view, post_detail_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/<int:post_id>', home_view),
+    path('', home_view),
+    path('posts/<int:post_id>', post_detail_view),
 ]
